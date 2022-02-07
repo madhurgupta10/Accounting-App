c58d40a416f7f9a28b915c792ce411d143167852
Madhur Gupta <madhurgupta10@gmail.com>
2022-02-07 18:19:22 +0530

    Add header UI

--- Native/Account/app/src/main/java/com/example/account/MainActivity.kt
+++ Native/Account/app/src/main/java/com/example/account/MainActivity.kt
@@ -11,27 +11,134 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.account.ui.theme.AccountTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AccountTheme {
-                TopAppBar()
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(color = MaterialTheme.colors.background)
+                ) {
+                    Box {
+                        TopAppBar()
+                        Header()
+                    }
+                }
             }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Header() {
+    Column(modifier = Modifier.padding(top = 100.dp, start = 20.dp)) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .width(200.dp)
+                .height(IntrinsicSize.Min)
+        ) {
+            NoInvoiceHeader()
+            Buttons(modifier = Modifier.align(Alignment.CenterVertically))
+        }
+    }
+}
+
+@Composable
+fun Buttons(modifier: Modifier) {
+    Row(modifier = modifier.padding(end = 20.dp)) {
+        FilterComposable(
+            modifier = Modifier.align(Alignment.CenterVertically)
+        )
+        AddNewButton(modifier = Modifier.align(Alignment.CenterVertically))
+    }
+}
+
+@Composable
+fun AddNewButton(modifier: Modifier) {
+    Box(
+        modifier = modifier
+            .clip(
+                RoundedCornerShape(
+                    90.dp,
+                    90.dp,
+                    90.dp,
+                    90.dp
+                )
+            )
+            .background(color = MaterialTheme.colors.primary)
+    ) {
+        Row(modifier = Modifier.padding(10.dp)) {
+            Box(
+                modifier = Modifier
+                    .size(35.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colors.onPrimary)
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_icon_plus),
+                    "New",
+                    tint = MaterialTheme.colors.primary,
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                )
+            }
+            Text(
+                "New", color = MaterialTheme.colors.onBackground,
+                style = MaterialTheme.typography.button,
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(start = 10.dp, end = 5.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun FilterComposable(modifier: Modifier) {
+    Row(modifier = modifier) {
+        Text(
+            "Filter", color = MaterialTheme.colors.onBackground,
+            style = MaterialTheme.typography.h3,
+        )
+        Icon(
+            painter = painterResource(R.drawable.ic_icon_arrow_down), "Filter List",
+            tint = MaterialTheme.colors.primary,
+            modifier = Modifier
+                .padding(top = 5.dp, start = 10.dp, end = 20.dp)
+                .align(Alignment.CenterVertically)
+        )
+    }
+}
+
+@Composable
+fun NoInvoiceHeader() {
+    Column {
+        Text(
+            "Invoices",
+            color = MaterialTheme.colors.onBackground,
+            style = MaterialTheme.typography.h1
+        )
+        Text(
+            "No invoices", color = MaterialTheme.colors.onBackground,
+            style = MaterialTheme.typography.h4
+        )
+    }
+}
+
 @Composable
 fun TopAppBar() {
     Row(
--- Native/Account/app/src/main/java/com/example/account/ui/theme/Type.kt
+++ Native/Account/app/src/main/java/com/example/account/ui/theme/Type.kt
@@ -2,27 +2,54 @@ package com.example.account.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.account.R
+
+val spartanFontFamily = FontFamily(
+    Font(R.font.spartan_bold, weight = FontWeight.Bold),
+    Font(R.font.spartan_extra_bold, weight = FontWeight.ExtraBold),
+    Font(R.font.spartan_light, weight = FontWeight.Light),
+    Font(R.font.spartan_extra_light, weight = FontWeight.ExtraLight),
+    Font(R.font.spartan_medium, weight = FontWeight.Medium),
+    Font(R.font.spartan_regular, weight = FontWeight.Normal),
+    Font(R.font.spartan_semi_bold, weight = FontWeight.SemiBold),
+    Font(R.font.spartan_thin, weight = FontWeight.Thin),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-        body1 = TextStyle(
-                fontFamily = FontFamily.Default,
-                fontWeight = FontWeight.Normal,
-                fontSize = 16.sp
-        )
-        /* Other default text styles to override
+    body1 = TextStyle(
+        fontFamily = spartanFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
+        fontFamily = spartanFontFamily,
+        fontWeight = FontWeight.Bold,
         fontSize = 14.sp
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = spartanFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+    h1 = TextStyle(
+        fontFamily = spartanFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    h3 = TextStyle(
+        fontFamily = spartanFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp
+    ),
+    h4 = TextStyle(
+        fontFamily = spartanFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
     )
-    */
+
 )
