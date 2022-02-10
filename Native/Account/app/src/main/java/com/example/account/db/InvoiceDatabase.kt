ab1372b387312d34e96ec44bc9ff04ae98b7a7a1
Madhur Gupta <madhurgupta10@gmail.com>
2022-02-11 04:35:22 +0530

    Update get db function name

--- Native/Account/app/src/main/java/com/example/account/db/InvoiceDatabase.kt
+++ Native/Account/app/src/main/java/com/example/account/db/InvoiceDatabase.kt
@@ -10,11 +10,7 @@ import com.example.account.model.InvoiceItem
 import com.example.account.model.converter.AddressConverter
 import com.example.account.model.converter.InvoiceItemConverter
 
-@Database(
-    entities = [Invoice::class, InvoiceItem::class],
-    version = 1
-)
-
+@Database(entities = [Invoice::class, InvoiceItem::class], version = 1)
 @TypeConverters(AddressConverter::class, InvoiceItemConverter::class)
 abstract class InvoiceDatabase : RoomDatabase() {
     abstract fun getInvoiceDao(): InvoiceDao
